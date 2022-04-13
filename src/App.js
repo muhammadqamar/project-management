e414d32addfc7b66666d13217118e3b8ddcae277
jabran <jabibutt786@gmail.com>
2022-04-13 17:59:09 +0500

    design header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from "react";
 import { VictoryPie } from "victory";
+import Navbar from "./components/navbar";
 import "./App.css";
 function App() {
   return (
-    <div className="App ">
-      <h1 className="mt-5 se">Active Project Management</h1>
-
-      <VictoryPie
-        colorScale={["tomato", "orange", "gold", "cyan", "navy"]}
-        data={[
-          { x: "Cats", y: 35 },
-          { x: "Dogs", y: 40 },
-          { x: "Birds", y: 55 },
-        ]}
-      />
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import navLogo from "../images/Logo.jpg";
+import "./navbar.css";
+const navbar = () => {
+  return (
+    <Navbar bg="primary" variant="dark" className="nav-main">
+      <img src={navLogo} className="logoImg" alt="image is here" />
+      <Container className="nav-container">
+        <Navbar.Brand href="#home" className="nav-text">
+          Active Project Management
+        </Navbar.Brand>
+        <Nav className="nav-links" ActiveKey="#">
+          <Nav.Link href="#ahvp" className="inner-nav-link">
+            ahvp
+          </Nav.Link>
+          <Nav.Link href="#ecvp" className="inner-nav-link">
+            ecvp
+          </Nav.Link>
+          <Nav.Link href="#fivp" className="inner-nav-link">
+            fivp
+          </Nav.Link>
+          <Nav.Link href="#pevp" className="inner-nav-link">
+            pevp
+          </Nav.Link>
+          <Nav.Link href="#pivp" className="inner-nav-link">
+            pivp
+          </Nav.Link>
+          <Nav.Link href="#rdvp" className="inner-nav-link">
+            rdvp
+          </Nav.Link>
+          <Nav.Link href="#snvp" className="inner-nav-link">
+            snvp
+          </Nav.Link>
+          <Nav.Link href="#urpr" className="inner-nav-link">
+            urpr
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default navbar;
